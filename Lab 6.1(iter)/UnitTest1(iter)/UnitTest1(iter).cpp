b32f00aafbc3bc86d96d6e5576a16fc47014a1c3
KovalDE <72768859+KovalDE@users.noreply.github.com>
2020-11-16 18:55:55 +0200

    Add files via upload

---
+++ Lab 6.1(iter)/UnitTest1(iter)/UnitTest1(iter).cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.1(iter)/Lab 6.1(iter).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1iter
+{
+	TEST_CLASS(UnitTest1iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[10] = {7, 3, -2, 1, 4, 0, 8, -4, 5, 6};
+			int c = Sum(a,10);
+			int d = Count(a, 10);
+
+			Assert::AreEqual(c, 18);
+
+			Assert::AreEqual(d,4);
+		}
+	};
+}
